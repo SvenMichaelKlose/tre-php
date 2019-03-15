e8c421e754e78becde07ef66e161ae1d25f03de9
Sven Michael Klose <pixel@hugbox.org>
2019-03-15 23:56:50 +0100

    Ignore *TRANSPILER-LOG*.

--- log-message.lisp
+++ log-message.lisp
@@ -1,8 +1,5 @@
-(defvar *standard-log* *standard-output*)
+(var *standard-log* *standard-output*)
 
-,(? *transpiler-log*
-    '(fn log-message (txt)
-       (%= nil (error_log txt))
-       txt)
-    '(defmacro log-message (txt)
-       txt))
+(fn log-message (txt)
+  (%= nil (error_log txt))
+  txt)
